3258aa1226d3cf2cbc6aec342f96e2da4afdeecd
S madhu kumar <madhukumar3110@gmail.com>
2025-11-06 00:01:43 +0530

    Delete switch.js

--- switch.js
+++
@@ -1,9 +0,0 @@
-color="blue"
-switch(color){
-    case "blue":
-    console.log("fav color")
-    break    
-    //    to execute only one condition we use break otherwise print all condition
-    case "maroon":
-    console.log("2nd fav color")
-}
